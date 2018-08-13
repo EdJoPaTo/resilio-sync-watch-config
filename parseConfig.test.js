08d49ca87badf0215fec30f38164ab2a6ab1f7f5
Edgar To <edjopato@gmail.com>
2018-08-13 18:21:59 +0200

    chore: migrate tests from jasmine to ava

---
+++ parseConfig.test.js
@@ -0,0 +1,91 @@
+const test = require('ava')
+const parseConfig = require('./parseConfig')
+
+const inputBasicConfig = {
+  basedir: '/location/',
+  folders: {}
+}
+
+test('creates minimal config', t => {
+  const out = parseConfig(inputBasicConfig)
+  t.truthy(out.device_name)
+  t.truthy(out.storage_path)
+  t.truthy(out.shared_folders)
+})
+
+test('creates storage_path', t => {
+  const out = parseConfig(inputBasicConfig)
+  t.is(out.storage_path, '/location/.sync')
+})
+
+test('has a device_name', t => {
+  const out = parseConfig(inputBasicConfig)
+  t.regex(out.device_name, /.+/)
+})
+
+test('has 0 shared folders', t => {
+  const out = parseConfig(inputBasicConfig)
+  t.deepEqual(out.shared_folders, [])
+})
+
+test('basedir without ending slash', t => {
+  const input = {
+    basedir: '/location',
+    folders: {}
+  }
+  const out = parseConfig(input)
+  t.is(out.storage_path, '/location/.sync')
+})
+
+test('basedir can be relative to pwd', t => {
+  const input = {
+    basedir: 'location',
+    folders: {}
+  }
+  const out = parseConfig(input)
+  t.is(out.storage_path, 'location/.sync')
+})
+
+test('basedir can be relative to home', t => {
+  const input = {
+    basedir: '~/location/',
+    folders: {}
+  }
+  const out = parseConfig(input)
+  const regex = new RegExp('/\\S+/\\S+/location/.sync')
+  t.regex(out.storage_path, regex)
+})
+
+const inputPassthrough = Object.assign({}, inputBasicConfig, {
+  passthrough: {
+    device_name: 'sparta',
+    what_the: 'hell'
+  }
+})
+
+
+test('adds passthrough', t => {
+  const out = parseConfig(inputPassthrough)
+  t.is(out.what_the, 'hell')
+})
+
+test('overrides device_name', t => {
+  const out = parseConfig(inputPassthrough)
+  t.is(out.device_name, 'sparta')
+})
+
+test('contains the shared folder', t => {
+  const input = Object.assign({}, inputBasicConfig, {
+    folders: {
+      tmp: 'XYZ'
+    }
+  })
+
+  const out = parseConfig(input)
+  t.deepEqual(out.shared_folders, [
+    {
+      dir: '/location/tmp',
+      secret: 'XYZ'
+    }
+  ])
+})
