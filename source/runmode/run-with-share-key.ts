00038203e6a505bea4180384e5e6908cb6854068
Edgar Toll <edjopato@gmail.com>
2019-07-28 02:54:19 +0200

    refactor(runWithShareKey): generalize loadConfigs

--- source/runmode/run-with-share-key.ts
+++ source/runmode/run-with-share-key.ts
@@ -3,7 +3,7 @@ import * as path from 'path'
 
 import {ResilioWithOwnConfigs} from '../resilio'
 
-import {OwnConfig, parseConfigs} from '../config'
+import {OwnConfig, parseConfigs, OwnConfigPart} from '../config'
 
 import {loadFromFile} from '../filesystem/own-config'
 import {watchDebounced} from '../filesystem/watch'
@@ -28,15 +28,7 @@ export async function runWithShareKey(resilio: ResilioWithOwnConfigs, basedir: s
   watchDebounced(
     async () => {
       try {
-        const content = await readdir(actualBasepath, {withFileTypes: true})
-        const configFiles = content
-          .filter(o => o.isFile())
-          .filter(o => o.name.endsWith('.json'))
-          .map(o => o.name)
-
-        console.log(new Date(), 'run with share key', 'found config files', configFiles)
-
-        const ownConfigParts = await loadFromFile(...configFiles.map(o => path.join(actualBasepath, o)))
+        const ownConfigParts = await loadConfigs(actualBasepath)
         await resilio.syncConfigs(...ownConfigParts, initConfig)
       } catch (error) {
         console.error(new Date(), 'run with share key', 'error while restarting', error)
@@ -44,4 +36,17 @@ export async function runWithShareKey(resilio: ResilioWithOwnConfigs, basedir: s
     },
     actualBasepath
   )
+}
+
+async function loadConfigs(basepath: string): Promise<readonly OwnConfigPart[]> {
+  const content = await readdir(basepath, {withFileTypes: true})
+        const configFiles = content
+          .filter(o => o.isFile())
+          .filter(o => o.name.endsWith('.json'))
+          .map(o => o.name)
+
+        console.log(new Date(), 'run with share key', 'found config files', configFiles)
+
+  const fullpathConfigFiles = configFiles.map(o => path.join(basepath, o))
+  return loadFromFile(...fullpathConfigFiles)
 }
