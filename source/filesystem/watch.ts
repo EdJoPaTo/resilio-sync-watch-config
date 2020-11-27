04acd1e2759c4109739652384339bc9234838542
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-11-27 17:08:08 +0100

    fix(filesystem): only watch json files
    ignore other changes
    
    new resilio versions seem to edit .fsprobe?

--- source/filesystem/watch.ts
+++ source/filesystem/watch.ts
@@ -8,7 +8,13 @@ import debounce from 'debounce-promise'
 type ChangeCallback = () => void
 
 export function watchDebounced(somethingChangedCallback: ChangeCallback, ...filesOrDirectories: readonly string[]): readonly FSWatcher[] {
-  const watchFunc = debounce(() => somethingChangedCallback(), 15000)
+  const debouncedUserFunc = debounce(() => somethingChangedCallback(), 15000)
+  const watchFunc = async (_event: string, filename: string) => {
+    if (filename.endsWith('.json')) {
+      await debouncedUserFunc()
+    }
+  }
+
   const watcher = filesOrDirectories
     .map(f => watch(f, {persistent: false}, watchFunc))
 
