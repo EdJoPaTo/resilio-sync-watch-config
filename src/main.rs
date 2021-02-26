ab1d0b823a13e8a0adf12f09d76c1f7fa4576e05
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2021-02-26 19:00:56 +0100

    feat(watch): initial watch mode without file watcher

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,8 @@ mod parse;
 mod resilio;
 mod share;
 
+const CONFIGS_FOLDER: &str = ".resilio-sync-watch-config/configs";
+
 fn main() {
     let matches = cli::build().get_matches();
 
@@ -86,6 +88,53 @@ fn main() {
                 sleep(Duration::from_millis(50));
             }
         }
+        ("watch", Some(matches)) => {
+            println!("Start in watch share mode...");
+            let share_secret = get_share_secret_from_arg(matches.value_of("share secret"))
+                .expect("Share secret could not be read or is invalid");
+
+            let mut safe_start = false; // TODO: as cli argument
+            let mut exits = false;
+
+            loop {
+                if exits {
+                    break;
+                }
+
+                // Ensure the last resilio is stopped before stuff is done.
+                resilio.stop().expect("failed to stop resilio");
+
+                if safe_start {
+                    let _ = fs::remove_dir_all(CONFIGS_FOLDER);
+                    safe_start = false;
+                }
+
+                let resilio_config = generate_watch_config(share_secret.to_owned(), basedir);
+                resilio.start_unsafe(&resilio_config);
+                println!("Resilio started.");
+
+                loop {
+                    if let Some(signal) = signals.pending().next() {
+                        println!("Received signal {:?}", signal);
+                        exits = true;
+                        break;
+                    }
+
+                    // TODO: check for changed files
+
+                    if !resilio
+                        .is_running()
+                        .expect("failed to check if resilio is still running")
+                    {
+                        println!("Resilio stopped unexpectedly! Restart with safe mode...");
+                        safe_start = true;
+                        break;
+                    }
+
+                    sleep(Duration::from_millis(50));
+                }
+            }
+        }
         (command, Some(_)) => todo!("Subcommand '{}' not yet implemented", command),
         _ => unimplemented!("You have to use a subcommand to run this tool"),
     }
@@ -109,4 +158,66 @@ fn get_share_secret_from_arg(secret_or_file_arg: Option<&str>) -> Option<String>
     } else {
         None
     }
+}
+
+fn get_currently_existing_config_file_names(folder: &str) -> Vec<String> {
+    match fs::read_dir(folder) {
+        Ok(dir_contents) => {
+            let mut list = Vec::new();
+            for entry in dir_contents {
+                if let Ok(entry) = entry {
+                    let is_file = entry.file_type().map_or(false, |o| o.is_file());
+                    if is_file {
+                        if let Ok(file_name) = entry.file_name().into_string() {
+                            if file_name.ends_with(".json") {
+                                list.push(file_name);
+                            }
+                        }
+                    }
+                }
+            }
+
+            list.sort();
+            list
+        }
+        Err(_) => vec![],
+    }
+}
+
+fn generate_watch_config(
+    config_share_secret: String,
+    base_folder: &str,
+) -> serde_json::Map<String, serde_json::Value> {
+    let config_files = get_currently_existing_config_file_names(CONFIGS_FOLDER);
+    println!(
+        "detected config files ({}): {:?}",
+        config_files.len(),
+        config_files
+    );
+    let mut config_files_in_config_dir = Vec::new();
+    for file in config_files {
+        config_files_in_config_dir.push(format!("{}/{}", CONFIGS_FOLDER, file));
+    }
+
+    let config_file_refs = config_files_in_config_dir
+        .iter()
+        .map(String::as_ref)
+        .collect::<Vec<_>>();
+
+    let mut own_config = match parse::read_and_merge(&config_file_refs) {
+        Ok(merged) => parse::apply_base_folder(merged, base_folder),
+        Err(err) => {
+            println!(
+                "WARNING: failed to parse config. Fall back to safe mode. Error: {}",
+                err
+            );
+            config::own::Config::default()
+        }
+    };
+
+    own_config
+        .folders
+        .insert(CONFIGS_FOLDER.to_string(), config_share_secret);
+
+    own_config.into_resilio_config()
 }
