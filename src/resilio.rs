9bb4f381c7915131b19faba0f720f80aa7d338a3
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2021-03-01 16:51:28 +0100

    refactor(resilio): represent one process not multiple

--- src/resilio.rs
+++ src/resilio.rs
@@ -12,118 +12,107 @@ use crate::config::resilio::Config;
 
 #[derive(Debug)]
 pub enum StopKind {
-    NotStarted,
-    StoppedNormally,
+    Normally,
     Killed,
 }
 
 #[derive(Debug)]
 pub struct Resilio {
     config_file: NamedTempFile,
-    binary: &'static str,
-    process: Option<Child>,
+    process: Child,
 }
 
 impl Resilio {
-    pub fn new(binary: &'static str) -> Self {
-        let config_file = tempfile::Builder::new()
+    pub fn new(binary: &str, config: &Config) -> Self {
+        Self::new_unsafe(binary, config)
+    }
+
+    pub fn new_unsafe<T>(binary: &str, config: &T) -> Self
+    where
+        T: ?Sized + serde::Serialize,
+    {
+        println!("Start Resilio...");
+
+        // TODO: create storage_path
+
+        let mut config_file = tempfile::Builder::new()
             .prefix("resilio-sync-watch-config-")
             .suffix(".conf")
             .tempfile()
             .expect("failed to create temporary config file");
 
-        Self {
-            config_file,
-            binary,
-            process: None,
-        }
-    }
-
-    pub fn is_running(&mut self) -> std::io::Result<bool> {
-        if let Some(handle) = &mut self.process {
-            let has_exitstatus = handle.try_wait()?.is_some();
-            Ok(!has_exitstatus)
-        } else {
-            Ok(false)
-        }
-    }
-
-    pub fn start(&mut self, config: &Config) {
-        self.start_unsafe(config)
-    }
-
-    pub fn start_unsafe<T>(&mut self, config: &T)
-    where
-        T: ?Sized + serde::Serialize,
-    {
         let mut contents = serde_json::to_string_pretty(&config)
             .expect("could not serialize resilio config to json");
         contents = contents.replace(": null", ": undefined");
 
-        self.stop().expect(
-            "failed to stop the running resilio process before attempting to start the new one",
-        );
-
-        self.config_file
+        config_file
             .write_all(contents.as_bytes())
             .expect("could not write resilio config to the temporary config file");
 
-        let handle = Command::new(self.binary)
+        let handle = Command::new(binary)
             .arg("--nodaemon")
             .arg("--config")
-            .arg(&self.config_file.path())
+            .arg(&config_file.path())
             .stdin(Stdio::null())
             .stdout(Stdio::null())
             .stderr(Stdio::null())
             .spawn()
             .expect("failed to start rslsync process");
-        self.process = Some(handle);
+        println!("Resilio started.");
+
+        Self {
+            config_file,
+            process: handle,
+        }
     }
 
-    pub fn stop(&mut self) -> std::io::Result<StopKind> {
+    pub fn is_running(&mut self) -> std::io::Result<bool> {
+        let has_exitstatus = self.process.try_wait()?.is_some();
+        Ok(!has_exitstatus)
+    }
+
+    fn stop(&mut self) -> std::io::Result<StopKind> {
         const MAX_DURATION: Duration = Duration::from_secs(10);
         const STEPS: u32 = 200;
         // TODO: wait for some divide function to be stable in const
         // const SINGLE_DURATION: Duration = MAX_DURATION.checked_div(STEPS).unwrap();
 
-        let stop_kind = if let Some(handle) = &mut self.process {
-            if handle.try_wait()?.is_none() {
-                #[allow(clippy::cast_possible_wrap)]
-                if let Err(err) = signal::kill(Pid::from_raw(handle.id() as i32), Signal::SIGTERM) {
-                    println!(
-                        "WARNING: failed to gracefully signal Resilio to stop: {}",
-                        err
-                    );
-                }
-            }
+        println!("Stop Resilio...");
 
-            let single_duration = MAX_DURATION.div(STEPS);
-            for _ in 0..STEPS {
-                if handle.try_wait()?.is_none() {
-                    sleep(single_duration);
-                } else {
-                    break;
-                }
+        if self.process.try_wait()?.is_none() {
+            #[allow(clippy::cast_possible_wrap)]
+            if let Err(err) = signal::kill(Pid::from_raw(self.process.id() as i32), Signal::SIGTERM)
+            {
+                println!(
+                    "WARNING: failed to gracefully signal Resilio to stop: {}",
+                    err
+                );
             }
+        }
 
-            // If its still not stopped use SIGKILL
-            if handle.try_wait()?.is_none() {
-                handle.kill()?;
-                StopKind::Killed
+        let single_duration = MAX_DURATION.div(STEPS);
+        for _ in 0..STEPS {
+            if self.process.try_wait()?.is_none() {
+                sleep(single_duration);
             } else {
-                StopKind::StoppedNormally
+                break;
             }
-        } else {
-            StopKind::NotStarted
-        };
+        }
 
-        self.process = None;
-        Ok(stop_kind)
+        // If its still not stopped use SIGKILL
+        if self.process.try_wait()?.is_none() {
+            self.process.kill()?;
+            println!("Resilio was killed as it took too long.");
+            Ok(StopKind::Killed)
+        } else {
+            println!("Resilio stopped normally.");
+            Ok(StopKind::Normally)
+        }
     }
 }
 
 impl Drop for Resilio {
     fn drop(&mut self) {
-        self.stop().expect("failed to stop the Resilio process");
+        self.stop().expect("failed to stop Resilio");
     }
 }
