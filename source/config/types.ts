4842aa16769617e48584e4a59144478f49eda9e6
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2021-03-05 18:36:52 +0100

    refactor(rust): remove typescript sources

--- source/config/types.ts
+++
@@ -1,7 +0,0 @@
-export type OwnConfigFolders = Readonly<Record<string, string>>
-
-export interface OwnConfig {
-  readonly basedir: string;
-  readonly folders: OwnConfigFolders;
-  readonly passthrough?: Readonly<Record<string, Readonly<unknown>>>;
-}
--- source/runmode/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './only-parse-file'
-export * from './run-with-share-key'
-export * from './run-with-specific-files'
